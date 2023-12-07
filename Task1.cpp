278d40272539c237bd36b6f301722f6bc1458dda
George D <georgedutra661@gmail.com>
2023-12-07 18:52:53 -0300

    start creating map

--- Task1.cpp
+++ Task1.cpp
@@ -43,20 +43,42 @@ vector<int> Map::nearestDMen(int origin, int numDMen) {
     return nearestDMen;
 }
 
+Map* generateMapQ1() {
+    Map* map = new Map(16);
+    
+    map->addEdge(1,2, 61);
+    map->addEdge(1,5, 98);
+    map->addEdge(2,3, 114);
+    map->addEdge(2,6, 115);
+    map->addEdge(3,4, 93);
+    map->addEdge(3,7, 87);
+    map->addEdge(4,8, 107);
+    map->addEdge(5,6, 105);
+    map->addEdge(5,9, 128);
+    map->addEdge(6,7, 70);
+    map->addEdge(6,10, 74);
+    map->addEdge(7,8, 103);
+    map->addEdge(7,11, 115);
+    map->addEdge(8,12, 116);
+    map->addEdge(9,10, 127);
+    map->addEdge(9,13, 96);
+    map->addEdge(10,11, 97);
+    map->addEdge(10,14, 116);
+    map->addEdge(11,12, 99);
+    map->addEdge(11,15, 104);
+    map->addEdge(12,16, 95);
+    map->addEdge(13,14, 123);
+    map->addEdge(14,15, 63);
+    map->addEdge(15,16, 127);
+
+    map->addDeliveryMan(1, 5, 10, 0);
+    map->addDeliveryMan(2, 5, 10, 0);
+
+    return map;
+}
+
 int main() {
     cout << "oi";
-
-    Map mapa(6);
-    
-    mapa.addEdge(0, 1, 10);
-    mapa.addEdge(0, 2, 20);
-    mapa.addEdge(1, 3, 30);
-    mapa.addEdge(1, 4, 20);
-    mapa.addEdge(2, 4, 90);
-    mapa.addEdge(3, 4, 100);
-    mapa.addEdge(4, 1, 40);
-    mapa.addEdge(4, 5, 60);
-
     // mapa.print();
 
     return 0;
