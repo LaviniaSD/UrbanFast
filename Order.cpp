e16e93d5de38dc02c797a8aa2bf425474e8acaa7
Isabela Yabe <103371464+IsabelaYabe@users.noreply.github.com>
2023-12-09 15:28:37 -0300

    Update Order.cpp

--- Order.cpp
+++ Order.cpp
@@ -1,62 +1,34 @@
-/**
- * @file Order.cpp
- * @brief Implementation file for the Order class.
- */
-
 #include "Order.h"
 
-Order::Order(int iOrderNumber, int iDestination, int iOrigin, bool bType):
-iOrderNumber(iOrderNumber),
-iDestination(iDestination),
-next(nullptr),
-pProducts(nullptr),
-bType(bType),
-iPrice(0),
-iWeight(0){
-    if(bType){
-        iOrigin = -1;
-    }
-}
-
+Order::Order(int iOrderNumber, int iDestination, int iOrigin, bool bType)
+    : iOrderNumber(iOrderNumber),
+      iDestination(iDestination),
+      iOrigin(iOrigin),
+      next(nullptr),
+      pProducts(nullptr),
+      bType(bType),
+      iPrice(0),
+      iWeight(0) {}
 
 Order::~Order() {
     // Free the dynamically allocated memory for the product items
     while (pProducts != nullptr) {
-        ProductQuantity* temp = pProducts;
+        Product* temp = pProducts;
         pProducts = pProducts->getNext();
         delete temp;
     }
 }
 
-void Order::addProducts(int iProductID, int iPriceProduct, int iQuantityProduct, int iWeightProduct) {
-    ProductQuantity* currentProduct = pProducts;
-
-    // Check if the product already exists in the order
-    while (currentProduct) {
-        // If the product exists, update its quantity, total price, and total weight
-        if (currentProduct->getProduct().getProductID() == iProductID) {
-            currentProduct->setQuantity(currentProduct->getQuantity() + iQuantityProduct);
-            iPrice += currentProduct->getProduct().getPrice() * iQuantityProduct;
-            iWeight += currentProduct->getProduct().getWeight() * iQuantityProduct;
-            return;
-        }
-        currentProduct = currentProduct->getNext();
+void Order::addProducts(int iProductID, int iPrice, int iQuantity, int iWeight) {
+    Product* newProduct = new Product(iProductID, iPrice, iQuantity, iWeight, nullptr);
+    if(!pProducts){
+        pProducts = newProduct;
     }
-    
-    // If the product doesn't exist, create a new product and add it to the order
-    Product newProduct(iProductID, iPriceProduct, iQuantityProduct, iWeightProduct,nullptr);
-    
-    ProductQuantity* newProductQuantity = new ProductQuantity(newProduct, iQuantityProduct);
-    iPrice += newProduct.getPrice() * iQuantityProduct;
-    iWeight += newProduct.getWeight() * iQuantityProduct;
-
-    // Link the new product to the existing products in the order
-    if (pProducts == nullptr) {
-        pProducts = newProductQuantity;
-    } else {
-        newProductQuantity->setNext(pProducts);
-        pProducts = newProductQuantity;
+    else{
+        newProduct->setNext(pProducts);
+        pProducts = newProduct;
     }
+    iPrice += iPrice * iQuantity;
+    iWeight += iWeight * iQuantity;
 }
-
 
