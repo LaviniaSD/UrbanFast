3d619ff73b259519df08b2a114f585a2485e9543
MarianaLima2000 <marianalimab.2000@gmail.com>
2023-12-09 21:18:58 -0300

    retorna com arquivo generico para testes das funcoes

---
+++ teste.cpp
@@ -0,0 +1,28 @@
+#include "Map.h"
+#include "Order.h"
+
+#include<iostream>
+using namespace::std;
+int main(){
+    Map* map = generateMapQ1();
+    Map mapClass = *map;
+
+    Order pedido(0,15,0,true);
+    pedido.addProducts(0,1,2,1);
+    pedido.addProducts(1,1,2,1);
+
+    cout<< "AAAAAAAH"<<endl;
+    ReturnFindRoutOpt* result =  mapClass.FindRouteOpt(pedido);
+    cout << "OLA" << endl;
+    int current = result->nearestDMan->getLocation();
+    cout << current << endl;
+    while (result->parents[current] != current){
+        cout << current << "<- ";
+        current = result->parents[current];
+    }
+    cout << current << endl;
+
+
+
+    return 0;
+}
